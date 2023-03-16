12522ca333aecfbd423cf4d353bd8c96453bf6a4
paraizovinicius <95001119+paraizovinicius@users.noreply.github.com>
2023-03-15 23:25:27 -0300

    Update 1C.R

--- 1C.R
+++ 1C.R
@@ -1,33 +1,31 @@
-library(ggplot2)
+#library(ggplot2)
+#library(gridExtra)
 
 
-# Carregando a base de dados
+# Importando os dados
 data <- read.table("http://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data", header = FALSE, sep = ",")
 
-
-
 # Renomeando as colunas
-colnames(data) <- c("type", "alc", "malic", "ash", "alkalinity", "mg", "phenols", "flav", "nonflav_phenols", "proan", "color_int", "hue", "OD280_OD315", "proline")
+colnames(data) <- c("type", "alcohol", "malic_acid", "ash", "alcalinity_of_ash", "magnesium", "total_phenols", "flavanoids", "nonflavanoid_phenols", "proanthocyanins", "color_intensity", "hue", "od280_od315_of_diluted_wines", "proline")
 
+# Separando os dados por tipo de vinho
+type1_data <- data[data$type == 1, ]
+type2_data <- data[data$type == 2, ]
+type3_data <- data[data$type == 3, ]
 
-
-# Dividindo a base de dados por tipo de vinho
-data1 <- data[data$type == 1, ]
-data2 <- data[data$type == 2, ]
-data3 <- data[data$type == 3, ]
-
-
-
-# Plotando um gráfico de distribuição de densidade para cada atributo por cada tipo de vinho
-for (col in 2:14) {
-  ggplot() + 
-    geom_density(data = data1, aes(x = data1[,col], y = ..density.., fill = "Tipo 1"), alpha = 0.3) +
-    geom_density(data = data2, aes(x = data2[,col], y = ..density.., fill = "Tipo 2"), alpha = 0.3) +
-    geom_density(data = data3, aes(x = data3[,col], y = ..density.., fill = "Tipo 3"), alpha = 0.3) +
-    ggtitle(paste("Distribuição de Densidade para", colnames(data)[col])) +
-    xlab("Valor do Atributo") +
-    ylab("Densidade") +
-    theme(legend.position = "top")
+# Função para criar o gráfico de densidade
+plot_density <- function(data, attribute) {
+  ggplot(data, aes(x = data[, attribute], fill = factor(type))) +
+    geom_density(alpha = 0.5) +
+    ggtitle(colnames(data)[attribute]) +
+    theme(plot.title = element_text(hjust = 0.5))
 }
 
-
+# Criando um gráfico de densidade para cada atributo por tipo de vinho
+for (i in 2:14) {
+  plot <- plot_density(type1_data, i) + facet_wrap(~ type)
+  print(plot)
+  
+  filename <- paste0("plot_", colnames(data)[i], ".png")  # nome do arquivo
+  ggsave(filename, plot, type = "png")  # salvando o plot em um arquivo PNG
+}
