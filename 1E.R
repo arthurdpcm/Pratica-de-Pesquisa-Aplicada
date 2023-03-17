0bd18026693b69e9040630fe25cf3b8768e49258
Arthur Duarte <61255233+arthurdpcm@users.noreply.github.com>
2023-03-17 20:29:50 -0300

    Delete 1E.R

--- 1E.R
+++
@@ -1,5 +0,0 @@
-
-wine <- read.table("http://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data", header = FALSE, sep = ",")
-
-# Gerando o gráfico de dispersão
-pairs(wine[, -1], main = "Gráfico de Dispersão dos Atributos", col = "blue")
